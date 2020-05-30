dd8ca33b6833414686c00fda195ab0b9f11f56c1
Assis Ferreira <assisrunny@hotmail.com>
2020-05-30 12:33:38 -0100

    WebSocketServer created

--- server.ts
+++ server.ts
@@ -1,9 +1,25 @@
 import { listenAndServe } from "https://deno.land/std/http/server.ts";
+import { WebSocket, WebSocketServer } from "https://deno.land/x/websocket/mod.ts";
 
+// html serve
 listenAndServe({port: 8000}, async(req) => {
 
 	if(req.method == "GET" && req.url == "/")
 		req.respond({ body: await Deno.open('index.html') });
+
+});
+
+// websocket serve
+const wss = new WebSocketServer(8080);
+
+wss.on("connection", function (ws: WebSocket) {
+
+	ws.on("message", function (message: string) {
+
+		console.log(message);
+		ws.send(message);
+
+	});
 
 });
 
